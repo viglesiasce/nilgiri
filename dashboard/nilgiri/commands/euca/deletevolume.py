0b6750ebad387c75873668c7532e26d7682ed681
shaon <shaon@MacBook-Pro.local>
2012-04-04 23:18:34 +0600

    volume feature added

---
+++ dashboard/nilgiri/commands/euca/deletevolume.py
@@ -0,0 +1,50 @@
+# Software License Agreement (BSD License)
+#
+# Copyright (c) 2009-2011, Eucalyptus Systems, Inc.
+# All rights reserved.
+#
+# Redistribution and use of this software in source and binary forms, with or
+# without modification, are permitted provided that the following conditions
+# are met:
+#
+#   Redistributions of source code must retain the above
+#   copyright notice, this list of conditions and the
+#   following disclaimer.
+#
+#   Redistributions in binary form must reproduce the above
+#   copyright notice, this list of conditions and the
+#   following disclaimer in the documentation and/or other
+#   materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#
+# Author: Imran Hossain Shaon mdshaonimran@gmail.com
+
+# file: nilgiri/dashboard/nilgiri/commands/euca/deletevolume.py
+
+import nilgiri.commands.nilgiricommand
+from boto.roboto.param import Param
+
+class DeleteVolume(nilgiri.commands.nilgiricommand.NilgiriCommand):
+
+    def main(self, query_volume_id):
+        conn = self.make_connection_cli()
+        return conn.delete_volume(volume_id=query_volume_id)
+
+    def main_cli(self, query_volume_id):
+        status = self.main(query_volume_id)
+        if status:
+            return "Done: " + query_volume_id
+        else:
+            return "Failed: " + query_volume_id
+
