3add681cf59b311b01f67da9ccb45af2edf3eb94
shaon <shaon@MacBook-Pro.local>
2012-04-14 02:52:52 +0600

    class order changed

--- dashboard/settings.py
+++ dashboard/settings.py
@@ -71,6 +71,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'django.core.context_processors.media',
     'django.core.context_processors.static',
     'django.contrib.messages.context_processors.messages',
+    'django.core.context_processors.csrf',
 )
 
 TEMPLATE_LOADERS = (
